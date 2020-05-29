ede69eac4b76fb73ebd3d1707df46eb23996e15e
edente <65434722+edente@users.noreply.github.com>
2020-05-29 02:22:29 -0500

    commited

---
+++ eLibrary/src/test/java/edu/mum/cs425/demowebapps/eLibrary/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs425.demowebapps.eLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eregistrar/src/test/java/edu/mum/cs425/demowebapps/eregistrar/EregistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs425.demowebapps.eregistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EregistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
